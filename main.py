aabeee56ff23df6947fca08a96b6675c32f8f758
WezerClear <hector.gallas@supinfo.com>
2024-04-29 13:07:47 +0200

    feat wordlist+argparse

--- main.py
+++ main.py
@@ -1,5 +1,66 @@
 import requests
 import sys
+import argparse
+import time
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog="sub-dir",
+        description="sub-dir",
+        epilog="dev: WezerClear / SGB  GitHub: https://github.com/WezerClear ",
+    )
+
+    parser.add_argument("-u", "--url", required=True, type=str, help="Target's url")
+
+    parser.add_argument(
+        "-S",
+        "--Subdomain",
+        required=False,
+        type=str,
+        help="Wordlist for Subdomains",
+    )
+    parser.add_argument(
+        "-D",
+        "--Directory",
+        required=False,
+        type=str,
+        help="Wordlist for directory",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--subdomain",
+        required=False,
+        type=str,
+        help="Select your words for Subdomains",
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        required=True,
+        type=str,
+        help="Select your words for directory",
+    )
+
+    parser.add_argument(
+        "--speed",
+        action="store_true",
+        required=False,
+        help="Choose the speed of the scan 1 to 5",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        required=False,
+        help="Active verbose",
+    )
+
+    args = parser.parse_args()
+
 
 sub_list = open("subdomains.txt").read()
 subdoms = sub_list.splitlines()
@@ -16,8 +77,6 @@ for sub in subdoms:
     else:
         print("Valid domain: ", sub_domains)
 
-import requests
-import sys
 
 sub_list = open("wordlist.txt").read()
 directories = sub_list.splitlines()
